b37cc81c675757ebd11cff839ef0e08cb43dda94
Kseniya <kseniya-belskaya@mail.ru>
2016-04-21 18:00:34 +0700

    modificate condition datetime

--- notification.py
+++ notification.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-import telebot
-import config
+import datetime
+
 import psycopg2
-import time
-from telebot import types
+import telebot
+
+import config
 
 connect = psycopg2.connect(database='d1eam1hffgoggg',
                            user='ravnyccawkzsbx',
@@ -13,29 +14,44 @@ cursor = connect.cursor()
 
 bot = telebot.TeleBot(config.token)
 
-
 if __name__ == '__main__':
     cursor.execute("select id_user,days from users")
+    # for i in range(10):
+    #     bot.send_message(52228717, 'I Love you very much! Take you money!')
 
     for id_user_days in cursor:
         id_user = id_user_days[0]
         days = id_user_days[1]
         print(id_user)
         print(days)
-       # bot.send_message(int(id_user), 'I Love you very much! Take you money!')
+        # bot.send_message(int(id_user), 'I Love you very much! Take you money!')
         cursor_ticket = connect.cursor()
         cursor_ticket.execute("select all_tickets.ticket_name," \
-                       " user_tickets.finish_date " \
-                       "from user_tickets " \
-                       "right outer join all_tickets " \
-                       "on user_tickets.id_ticket=all_tickets.id_ticket " \
-                       "where id_user =" + str(id_user))
+                              " user_tickets.finish_date " \
+                              "from user_tickets " \
+                              "right outer join all_tickets " \
+                              "on user_tickets.id_ticket=all_tickets.id_ticket " \
+                              "where id_user =" + str(id_user))
         for ticket_data in cursor_ticket:
-            ticket_name=ticket_data[0]
-            finish_date=ticket_data[1]
-            print(ticket_name,finish_date)
-            if (int(finish_date)-int(days) <= int(time.strftime("%d")) and
-            int(time.strftime("%d")) < int(finish_date)):
-                bot.send_message(int(id_user), "Необходимо оплатить квитанцию \""+ ticket_name.strip() + "\"")
+            ticket_name = ticket_data[0]
+            finish_date = ticket_data[1]
 
-connect.close()
+            date = datetime.date.today()
+            month = date.month
+            year = date.year
+            day_of_date = datetime.date(year, month, int(finish_date))
+            day_of_date_before_month = datetime.date(year, month, int(finish_date))
+
+            print(ticket_name, finish_date)
+            # if (int(finish_date)-int(days) <= int(time.strftime("%d")) and
+            # int(time.strftime("%d")) <= int(finish_date)):
+            if int(finish_date) <= int(days):
+                if (day_of_date - datetime.timedelta(days=int(days)) <= date) and (date <= day_of_date):
+                    bot.send_message(int(id_user), "Необходимо оплатить квитанцию \"" + ticket_name.strip() + "\"")
+
+            if int(finish_date) > int(days):
+                if (day_of_date_before_month - datetime.timedelta(days=int(days) <= date)) and (
+                    date <= day_of_date_before_month):
+                    bot.send_message(int(id_user), "Необходимо оплатить квитанцию \"" + ticket_name.strip() + "\"")
+
+                connect.close()
